48330142e6292beb530ae805a0b1948d0d78c126
Martino Zanetti <98329317+martinozanetti@users.noreply.github.com>
2022-10-28 14:10:35 +0200

    Add files via upload

---
+++ sito_CA/inc/head.js
@@ -0,0 +1,7 @@
+document.write('<head>');
+document.write('    <title>Corrente Alternativa</title>');
+document.write('    <link rel="shortcut icon" href="img/logo.png">');
+document.write('    <meta charSet="UTF-8">');
+document.write('    <meta name="viewport" content="width=device-width, initial-scale=1">');
+document.write('    <link rel="stylesheet" href="css/w3style.css">');
+document.write('</head>');
